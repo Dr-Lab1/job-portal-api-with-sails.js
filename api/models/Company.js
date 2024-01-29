cd4768d857899fbf0e331c35169bf654b43f07e1
Dr-Lab1 <jonathankukwabantu120302@gmail.com>
2024-01-29 15:00:59 +0100

    Company's Model - 29.01.2024 at 15:00

---
+++ api/models/Company.js
@@ -0,0 +1,24 @@
+module.exports = {
+
+    attributes: {
+        name: {
+            type: "string",
+            required: true
+        },
+        description: {
+            type: 'string',
+            required: true
+        }, 
+        country: {
+            typ: 'string'
+        },
+        city: {
+            type: 'string',
+            required: true
+        },
+        address: {
+            type: 'string'
+        }
+    }
+
+}
