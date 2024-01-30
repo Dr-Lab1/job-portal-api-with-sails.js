42c74cc1d9dafd1904f91c1c6e130c9521690d8d
Dr-Lab1 <jonathankukwabantu120302@gmail.com>
2024-01-30 11:09:54 +0100

    CompanyController create fx async await - 30.01.2024 at 11:09

--- api/controllers/CompanyController.js
+++ api/controllers/CompanyController.js
@@ -1,34 +1,24 @@
 module.exports = {
     
     async create(req, res) {
-        
         try {
-            // get all params from the request
             let params = req.allParams();
 
-            // verified the required params
-            if(!params.name) {
-                return res.badRequest({err: 'name is required'});
-            }
-            if(!params.description) {
-                return res.badRequest({err: 'description is required'});
-            }
-            
-            // store datas in db
-            const results = await Company.create(
-                {
-                    "name": params.name,
-                    "description": params.description,
-                    "country": params.country,
-                    "city": params.city,
-                    "address": params.address,
-                }
-            );
+            if(!params.name)
+                res.badRequest({err : "Name field is required"});
+            if(!params.description)
+                res.badRequest({err : "Description field is required"});
 
+            const results = await Company.create({
+                "name": params.name,
+                "description": params.description,
+                "country": params.country,
+                "city": params.city,
+                "address": params.address,
+            });
             return res.ok(results);
-
         } catch (err) {
-            return res.serverError(err); 
+            return res.serverError(err);
         }
     },
     
